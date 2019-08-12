33e7f11baf40a7250d7fcc14a924f69f1481c979
jairodelgado <jrdelgado@uci.cu>
2019-08-12 09:43:12 -0400

    add user and connection models

---
+++ db/models/connection.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = function(Sequelize, DataTypes) {
+  var User = Sequelize.define('Connection', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    }, {
+    classMethods: {
+      associate: function(models) {
+        models.Connection.belongsTo(models.User);
+        models.Connection.belongsTo(models.User);
+      }
+    },
+    freezeTableName: true,
+    timestamps: false
+  });
+  return User;
+};
---
+++ db/models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = function(Sequelize, DataTypes) {
+  var User = Sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: {
+          msg: "you must provide a name with only letters"
+        },
+        len: {
+          args: [5, 20],
+          msg: "the name must have at least 5 characters long and no more than 20"
+        }
+      }
+    }, {
+    classMethods: {
+    },
+    freezeTableName: true,
+    timestamps: false
+  });
+  return User;
+};
