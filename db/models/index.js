5cd0df23fafa2bf1d2b5b3211abde969722483f4
jairodelgado <jrdelgado@uci.cu>
2019-08-12 09:19:11 -0400

    add database models loader

---
+++ db/models/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var env = process.env.NODE_ENV || "development";
+var config = require(path.join(__dirname, '../..', 'config', 'database.json'))[env];
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var models = {};
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) => {
+    return file !== "index.js";
+  })
+  .forEach((file) => {
+    var model = sequelize.import(path.join(__dirname, file));
+    models[model.name] = model;
+  });
+
+Object.keys(models).forEach((modelName) => {
+  if ("associate" in models[modelName]) {
+    models[modelName].associate(models);
+  }
+});
+
+module.exports = models;
