da14f22de1a36ced8c4b4db6c544e4d6155187f2
jairodelgado <jrdelgado@uci.cu>
2019-08-12 09:33:25 -0400

    add server routes loader

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 var path = require('path');
 
 module.exports = (app) => {
-	app.use('/api/roles', require('./role')(app.models));
 	app.use('/api/users', require('./user')(app.models));
-	app.use('/api/issues', require('./issue')(app.models));
-	app.use('/api/replies', require('./reply')(app.models));
 
 	app.use((req, res) => {
 		res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
