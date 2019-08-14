2e9d7165d0482e0f8d6b4bbe24854480546ca72b
jairodelgado <jrdelgado@uci.cu>
2019-08-14 16:56:48 -0400

    fixup update react routes

--- client/index.js
+++ client/index.js
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 
 import App from './src/app/app.component.jsx';
+import Index from './src/app/index.component.jsx';
+
 
 import User from './src/user/user.component.jsx';
 import UserCreate from './src/user/user.create.component.jsx';
 import UserDetails from './src/user/user.details.component.jsx';
 
+import Connection from './src/connection/connection.component.jsx';
+import ConnectionCreate from './src/connection/connection.create.component.jsx';
+import ConnectionGraph from './src/connection/connection.graph.component.jsx';
+
 var application = (<App>
   <Router history={browserHistory}>
-      <Route path="/users" component={User}/>
-      <Route path="/users/create" component={UserCreate}/>
-      <Route path="/users/details/:id" component={UserDetails}/>
+    <Route path="/" component={Index}/>
+    <Route path="/users" component={User}/>
+    <Route path="/create/users" component={UserCreate}/>
+    <Route path="/details/users/:id" component={UserDetails}/>
+    
+    <Route path="/connections/:id" component={Connection}/>
+    <Route path="/create/connections/:id" component={ConnectionCreate}/>
+    <Route path="/graph" component={ConnectionGraph}/>
   </Router>
 </App>);
 
