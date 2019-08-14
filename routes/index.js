fae79f40f6c9dd9a97cd8ea9d8967a7576de1722
jairodelgado <jrdelgado@uci.cu>
2019-08-14 16:31:13 -0400

    fixup update connection routes in inde file

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 module.exports = (app) => {
 	app.use('/api/users', require('./user')(app.models));
+	app.use('/api/connections', require('./connection')(app.models));
 
 	app.use((req, res) => {
 		res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
